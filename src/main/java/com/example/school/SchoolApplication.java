955a479e76f3ec0d59c7ad32a40495d98cd27e71
sembekov <76952248+sembekov@users.noreply.github.com>
2024-04-15 07:19:17 +0500

    Initial commit

---
+++ src/main/java/com/example/school/SchoolApplication.java
@@ -0,0 +1,14 @@
+package com.example.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.*;
+
+@SpringBootApplication
+public class SchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolApplication.class, args);
+    }
+}
